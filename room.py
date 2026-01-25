b9e90802eb57afb6711db64bcb4e988796749194
andrea-evrard <andrea.evrard@edu.esiee.fr>
2026-01-25 01:57:03 +0000

    amélioration room

--- room.py
+++ room.py
@@ -1,4 +1,5 @@
 # Define the Room class.
+""" Module for room representation in the game. """
 
 class Room:
     """
@@ -9,7 +10,7 @@ class Room:
             description (str): The description of the room.
             exits (dictionnaire): The different exits possible.
     """
-    # Define the constructor. 
+    # Define the constructor.
     def __init__(self, name, description):
         self.name = name
         self.description = description
@@ -20,19 +21,24 @@ class Room:
         self.locked_message = "La porte est fermée à clé."
         self.entry_condition = None  # Fonction de vérification pour entrer
         self.entry_denied_message = "Vous ne pouvez pas entrer ici."
-    
+
     # Define the get_exit method.
     def get_exit(self, direction):
-
+        """ Get the exit in the given direction.
+        Args:
+            direction (str): The direction of the exit.
+        Returns:     
+            Room: The room in the given direction."""
         # Return the room in the given direction if it exists.
         if direction in self.exits.keys():
             return self.exits[direction]
         else:
             return None
-    
+
     # Return a string describing the room's exits.
     def get_exit_string(self):
-        exit_string = "Sorties: " 
+        """ Returns a string describing the room's exits."""
+        exit_string = "Sorties: "
         for exit in self.exits.keys():
             if self.exits.get(exit) is not None:
                 exit_string += exit + ", "
@@ -41,6 +47,7 @@ class Room:
 
     # Return a long description of this room including exits.
     def get_long_description(self):
+        """ Returns a long description of this room including exits."""
         return f"\nVous êtes dans{self.description}\n\n{self.get_exit_string()}\n"
 
     # Display the items in the room.
@@ -52,10 +59,9 @@ class Room:
         if not self.items:
             print("\nIl n'y a rien ici.\n")
             return False
-        
+
         else:
             print("\nLa pièce contient :")
             for item_name, item in self.items.items():
                 print(f"- {item}")
             return True
-
